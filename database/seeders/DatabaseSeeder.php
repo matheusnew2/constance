7a7e8cb05a2b977673bfa751bd97415d3a6db102
Matheus Felipe Alves <matheus01felipe@hotmail.com>
2025-12-04 16:33:14 -0300

    Criação de seed para popular incialmente o banco de dados

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\FornecedorModel;
+use App\Models\PedidoModel;
+use App\Models\PedidoProdutoModel;
+use App\Models\ProdutoModel;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,11 +19,38 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $fornecedores = FornecedorModel::factory(3)->create();
+        foreach($fornecedores as $fornecedor)
+        {
+            $produtosCollection = ProdutoModel::factory(3)->create([
+                'fornecedor_id' => $fornecedor->id,
+                'nome' => 'produto'
+            ]);
+            $qtd_produtos = rand(1,3);
+            $produtos = [];
+            $valor_pedido = 0;
+            for ($i=0; $i < $qtd_produtos; $i++) { 
+                $quantidade = rand(1,5);
+                $produtos[] = [
+                    'produto_id' => $produtosCollection[$i]->id,
+                    'valor_unitario' => $produtosCollection[$i]->preco,
+                    'quantidade' => $quantidade
+                ];
+                $valor_pedido += $produtosCollection[$i]->preco * $quantidade;
+            }
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+            $pedido = PedidoModel::factory()->create([
+                'fornecedor_id' => $fornecedor->id,
+                'valor' => $valor_pedido,
+                'observacao' => 'obs',
+                'status' => 'pendente',
+            ]);
+            foreach($produtos as $produto)
+            {
+                PedidoProdutoModel::factory()->create(array_merge(['pedido_id' => $pedido->id],$produto));
+            }
+
+            
+        }
     }
 }
